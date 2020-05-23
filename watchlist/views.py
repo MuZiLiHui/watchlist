6538a8c2b58ef97a8b9abe0b564292b4794f652c
MuZiLiHui <xinshanmuzi@gmail.com>
2020-05-23 14:50:07 +0800

    Ready to deploy

--- watchlist/views.py
+++ watchlist/views.py
@@ -6,30 +6,6 @@ from flask_login import login_user, login_required, logout_user, current_user
 from watchlist import app, db
 from watchlist.models import User, Movie
 
-'''
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if not current_user.is_authenticated:
-            return redirect(url_for('index'))
-
-        title = request.form['title']
-        year = request.form['year']
-
-        if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('index'))
-
-        movie = Movie(title=title, year=year)
-        db.session.add(movie)
-        db.session.commit()
-        flash('Item created.')
-        return redirect(url_for('index'))
-
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
-'''
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # 判断是否是 POST 请求
